ef5fbda17b6111ad502ef677ed9554a6c6fb48af
Swakhil <mattapalliswakhil@gmail.com>
2021-07-07 09:23:21 +0530

    code alignment

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ $message .= "\n\nTo send a reply, please click-> $email";
       <div class="container">
         <div class="row align-items-center">
           <div class="col-11 col-xl-2">
-            <a href="#home"><img src="images/company_logo.png" height="50px" width="200px"></a>
+            <a href="https://festech.in"><img src="images/company_logo.png" height="50px" width="200px"></a>
           </div>
           <div class="col-12 col-md-10 d-none d-xl-block">
             <nav class="site-navigation position-relative text-right" role="navigation">
@@ -89,28 +89,25 @@ $message .= "\n\nTo send a reply, please click-> $email";
     </header>
   </div>
 
-    <div  id="home" class="site-blocks-cover overlay" style="background-image: url(images/hero_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-      <div class="container">
-        <div class="row align-items-center justify-content-center text-center">
-
-          <div class="col-md-12" data-aos="fade-up" data-aos-delay="400">
-                        
-            <div class="row justify-content-center mb-4">
-              <div class="col-md-8 text-center">
-                <br><br><br><br><br>
-                <h1 style="color:white;"> We <span class="typed-words"></span></h1>
-                <h1 style="color:white;">Websites, Web & Mobile Apps</h1>
-                <p class="lead mb-5"><b>We are here for you</b></p>
-                <div><a href="#contact" class="btn btn-primary btn-md">Contact Us</a></div>
-              </div>
+  <div  id="home" class="site-blocks-cover overlay" style="background-image: url(images/hero_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row align-items-center justify-content-center text-center">
+        <div class="col-md-12" data-aos="fade-up" data-aos-delay="400">         
+          <div class="row justify-content-center mb-4">
+            <div class="col-md-8 text-center">
+              <br><br><br><br><br>
+              <h1 style="color:white;"> We <span class="typed-words"></span></h1>
+              <h1 style="color:white;">Websites, Web & Mobile Apps</h1>
+              <p class="lead mb-5"><b>We are here for you</b></p>
+              <div><a href="#contact" class="btn btn-primary btn-md">Contact Us</a></div>
             </div>
-
           </div>
         </div>
       </div>
-    </div>  
+    </div>
+  </div>  
 
-  <!--Our Latest Releases-->
+  <!--Services We Offer-->
   <section id="services" class="site-section">
       <div class="container">
         <div class="row mb-5 justify-content-center">
@@ -162,40 +159,40 @@ $message .= "\n\nTo send a reply, please click-> $email";
       </div>
   </section>
 
-<!--About Us-->
-<section id="about-us" class="site-section bg-light">
-  <div class="container">
-    <div class="row mb-5 justify-content-center">
-      <div class="col-md-8 text-center">
-        <h2 class="text-black h1 site-section-heading text-center">About Us</h2>
+  <!--About Us-->
+  <section id="about-us" class="site-section bg-light">
+    <div class="container">
+      <div class="row mb-5 justify-content-center">
+        <div class="col-md-8 text-center">
+          <h2 class="text-black h1 site-section-heading text-center">About Us</h2>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="row">
-          <div class="col-md-6">
-            <h3>Mission</h3>
-            <p>To provide viable technical solutions in the form of mobile applications to build and support your digital network. We aim to bring forth the best service for years to come.</p>
-          </div>
-          <div class="col-md-6">
-            <h3>Vision</h3>
-            <p>To offer quality service and build value for clients by becoming an integral web and mobile applications service provider.</p>
-          </div>
-          <div class="col-md-6">
-            <h3>Future</h3>
-            <p>To be the first choice of clients to help meet their goals through our services, people, and solutions. While using modern and highly sophisticated technologies to build solutions.</p>
-          </div>
-          <div class="col-md-6">
-            <h3>Scope for Learning</h3>
-            <p>We are a team of 7 +uber-cool, enthusiastic, computer geeks who learn, teach and grow together. Our only motto is “Do better what you can do the best”. ##SO WE ARE OUT HERE LOOKING FOR ENJOYING EACH DAY AS FUN AS A FESTIVAL WITH TECHNOLOGY.</p>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-6">
+              <h3>Mission</h3>
+              <p>To provide viable technical solutions in the form of mobile applications to build and support your digital network. We aim to bring forth the best service for years to come.</p>
+            </div>
+            <div class="col-md-6">
+              <h3>Vision</h3>
+              <p>To offer quality service and build value for clients by becoming an integral web and mobile applications service provider.</p>
+            </div>
+            <div class="col-md-6">
+              <h3>Future</h3>
+              <p>To be the first choice of clients to help meet their goals through our services, people, and solutions. While using modern and highly sophisticated technologies to build solutions.</p>
+            </div>
+            <div class="col-md-6">
+              <h3>Scope for Learning</h3>
+              <p>We are a team of 7 +uber-cool, enthusiastic, computer geeks who learn, teach and grow together. Our only motto is “Do better what you can do the best”. ##SO WE ARE OUT HERE LOOKING FOR ENJOYING EACH DAY AS FUN AS A FESTIVAL WITH TECHNOLOGY.</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<!--Our Deployed Projects-->
+  <!--Our Clients-->
   <section id="clients" class="site-section">
     <div class="container">
       <div class="row mb-5 justify-content-center">
@@ -226,87 +223,85 @@ $message .= "\n\nTo send a reply, please click-> $email";
     </div>
   </section>
 
-<!--Contact Section -->
-<section id="contact" class="site-section bg-light">
-  <div class="container">
-    <div class="row mb-5 justify-content-center">
-      <div class="col-md-8 text-center">
-        <h2 class="text-black h1 site-section-heading text-center">Contact Us</h2>
-        <p>Send us a message and we would love to connect with you !</p>
+  <!--Contact Section -->
+  <section id="contact" class="site-section bg-light">
+    <div class="container">
+      <div class="row mb-5 justify-content-center">
+        <div class="col-md-8 text-center">
+          <h2 class="text-black h1 site-section-heading text-center">Contact Us</h2>
+          <p>Send us a message and we would love to connect with you !</p>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="row">
-          <div class="col-md-3">
-            <div class="contact-address">
-              <h4>Address</h4>
-              <p><span class="glyphicon glyphicon-map-marker"></span>Hyderabad, India</p>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-3">
+              <div class="contact-address">
+                <h4>Address</h4>
+                <p><span class="glyphicon glyphicon-map-marker"></span>Hyderabad, India</p>
+              </div>
+              <div class="contact-phone">
+                <i class="ion-ios-telephone-outline"></i>
+                <h4>Phone Number</h4>
+                <p><a href="https://api.whatsapp.com/send?phone=+917075003026">+91 7075003026</a></p>
+              </div>
+              <div class="contact-email">
+                <i class="ion-ios-email-outline"></i>
+                <h4>Email</h4>
+                <p><a href="mailto:festechsolutions@gmail.com">festechsolutions@gmail.com</a></p>
+              </div>
             </div>
-            <div class="contact-phone">
-              <i class="ion-ios-telephone-outline"></i>
-              <h4>Phone Number</h4>
-              <p><a href="https://api.whatsapp.com/send?phone=+917075003026">+91 7075003026</a></p>
-            </div>
-            <div class="contact-email">
-              <i class="ion-ios-email-outline"></i>
-              <h4>Email</h4>
-              <p><a href="mailto:festechsolutions@gmail.com">festechsolutions@gmail.com</a></p>
-            </div>
-          </div>
-          <div class="col-md-9 ml-auto">
-            <div class="col-md-12 form">
-              <form action="" method="post" role="form" class="contactForm">
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" required/>
+            <div class="col-md-9 ml-auto">
+              <div class="col-md-12 form">
+                <form action="" method="post" role="form" class="contactForm">
+                  <div class="form-row">
+                    <div class="form-group col-md-6">
+                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" required/>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" required/>
+                    </div>
+                    <div class="form-group col-md-12">
+                      <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" required/>
+                    </div>
+                    <div class="form-group col-md-12">
+                      <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message" required></textarea>
+                    </div>
+                    <div class=" col-md-12 text-center"><input type="submit" name="submit" value="Contact Us" class="btn btn-primary btn-md"></div>
                   </div>
-                  <div class="form-group col-md-6">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" required/>
-                  </div>
-                  <div class="form-group col-md-12">
-                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" required/>
-                  </div>
-                  <div class="form-group col-md-12">
-                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message" required></textarea>
-                  </div>
-                  <div class=" col-md-12 text-center"><input type="submit" name="submit" value="Contact Us" class="btn btn-primary btn-md"></div>
-                </div>
-                <?php
-                if (isset ($_POST['submit']))
-                {
-                  if(mail($to,$subject,$message,$headers))
+                  <?php
+                  if (isset ($_POST['submit']))
                   {
-                    echo '<script type="text/javascript">';
-                    echo 'setTimeout(function () { swal("Hurray!","Your response has been received, Relax and sit back we will contact you soon.","success");';
-                    echo '}, 1000);</script>';
+                    if(mail($to,$subject,$message,$headers))
+                    {
+                      echo '<script type="text/javascript">';
+                      echo 'setTimeout(function () { swal("Hurray!","Your response has been received, Relax and sit back we will contact you soon.","success");';
+                      echo '}, 1000);</script>';
+                    }
+                    else{
+                      echo '<script type="text/javascript">';
+                      echo 'setTimeout(function () { swal("Oops","There is an error, our engineers are working on the issue. Apologies for the Inconvenience","error");';
+                      echo '}, 1000);</script>';
+                    }
                   }
-                  else{
-                    echo '<script type="text/javascript">';
-                    echo 'setTimeout(function () { swal("Oops","There is an error, our engineers are working on the issue. Apologies for the Inconvenience","error");';
-                    echo '}, 1000);</script>';
-                  }
-                }
-              ?>
-              </form>
+                  ?>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-</section>
+  </section>
 
-    <!--Footer Section-->
-    <footer id="footer" class="site-footer">
-      <div class="container">
-       <div class="row justify-content-center">
-           <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>  Festech solutions, All rights reserved | made in association with <a href="https://colorlib.com" target="_blank" >Colorlib</a></p>
-        </div> 
-      </div>
-    </footer>
-</div>
+  <!--Footer Section-->
+  <footer id="footer" class="site-footer">
+    <div class="container">
+     <div class="row justify-content-center">
+         <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>  Festech solutions, All rights reserved | made in association with <a href="https://colorlib.com" target="_blank" >Colorlib</a></p>
+      </div> 
+    </div>
+  </footer>
 
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="js/jquery-migrate-3.0.1.min.js"></script>
@@ -336,10 +331,4 @@ $message .= "\n\nTo send a reply, please click-> $email";
   <script src="js/main.js"></script>
     
   </body>
-  <style>
-    .mb-5{
-      padding: 7em 0;
-    padding-bottom: 0;
-    }
-  </style>
 </html>
